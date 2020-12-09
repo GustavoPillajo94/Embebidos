029ea98c2f814cc70f9911b04cc273d645b0a0f2
GustavoPillajo94 <74869020+GustavoPillajo94@users.noreply.github.com>
2020-12-09 01:01:27 -0500

    Add files via upload

---
+++ Pillajo_Gustavo_Embebidos_Deber5.ino
@@ -0,0 +1,38 @@
+#include<Servo.h>
+Servo servo_1;
+int mod=0;
+
+void setup() {
+  Serial.begin(9600);
+  attachInterrupt(digitalPinToInterrupt(2),modos,RISING);
+ servo_1.attach(9, 500, 2500);
+//  servo_1.write(pos);
+}
+
+void loop() {
+  if(mod==1){
+    Serial.print("Giro del motor");
+    if(Serial.available()<0){
+    int giro=Serial.read();
+    servo_1.write(giro);
+    }
+    }
+delay(1000);
+}
+void modos(){
+  switch(mod){
+  case 0:
+  Serial.print("inicio del sistema");
+  mod++;
+  break;
+  case 1:
+  Serial.print("giro del motor");
+  mod++;
+  break;
+  case 2:
+  Serial.print("fin del programa");
+  mod=0;
+  break;
+  }
+  }
+  
